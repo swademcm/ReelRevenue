8e814da2cb5f591a10b186065d117c0c0987989a
swade <156927269+swademcm@users.noreply.github.com>
2025-09-22 13:46:27 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def signup():
     return redirect('/')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=True)
